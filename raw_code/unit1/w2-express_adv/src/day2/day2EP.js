f21ade050e4bf9675d6b7fca8e08dc49c4d53d70
Connor Wehmeyer <114667779+connordw-sa@users.noreply.github.com>
2023-09-11 13:37:59 +0200

    .

---
+++ raw_code/unit1/w2-express_adv/src/day2/day2EP.js
@@ -0,0 +1,17 @@
+// Homework
+// Epicode Blog API
+
+// Complete all the homeworks from last week (including extras 😜), then deploy it on cloud.
+
+// ☁️ CLOUD
+// We want to make applications available for the general public and production ready 🚗⚡
+
+// Deploy everything on RAILWAY/CYCLIC/HEROKU and VERCEL, with CLOUDINARY as CDN
+
+// ⚠️ If you have troubles with running on cyclic/heroku remind to configure start script in package.json
+
+// ⚠️ ⚠️ Remind to use environment variables both locally and remotely
+
+// ⚠️ ⚠️ ⚠️ Configure CORS properly to make them work together
+
+// ⚠️ ⚠️ ⚠️⚠️ CI=false as environment variable on Vercel lets you execute your React code even though there are some warnings
